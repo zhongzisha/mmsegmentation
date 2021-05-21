15c67248f980fad7039ee976965a915f3ab2148c
zhongzisha <zhongzisha@outlook.com>
2021-05-21 21:56:38 +0800

    begin to train 3classes

---
+++ configs/deeplabv3plus/deeplabv3plus_r50-d8_512x512_80k_gd_mc3classes.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/deeplabv3plus_r50-d8.py', '../_base_/datasets/gd_mc3classes.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+model = dict(
+    decode_head=dict(num_classes=3), auxiliary_head=dict(num_classes=3))
