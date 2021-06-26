7bf2b4c5678710c81ba02e9ae7692089ef76e2dd
zhongzisha <zhongzisha@outlook.com>
2021-06-26 23:02:12 +0800

    add many configs

---
+++ configs/unet/pspnet_unet_s5-d16_256x256_40k_gd_line512new_lr0.01.py
@@ -0,0 +1,80 @@
+_base_ = [
+    '../_base_/models/pspnet_unet_s5-d16.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_40k.py'
+]
+
+# dataset settings
+dataset_type = 'CustomDataset'
+data_root = 'data/gd_line512new'
+classes = ('bg', 'line')
+palette = [[0, 0, 0], [255, 255, 255]]
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_scale = (512, 512)
+crop_size = (256, 256)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(1.0, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type='RepeatDataset',
+        times=40000,
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            classes=classes,
+            palette=palette,
+            img_dir='train/images',
+            ann_dir='train/annotations',
+            split='train.txt',
+            pipeline=train_pipeline)),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        classes=classes,
+        palette=palette,
+        img_dir='val/images',
+        ann_dir='val/annotations',
+        split='val.txt',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        palette=palette,
+        data_root=data_root,
+        img_dir='val/images',
+        ann_dir='val/annotations',
+        split='val.txt',
+        pipeline=test_pipeline))
+
+model = dict(test_cfg=dict(crop_size=(256, 256), stride=(170, 170)))
+evaluation = dict(metric='mDice')
+optimizer = dict(lr=0.01)
