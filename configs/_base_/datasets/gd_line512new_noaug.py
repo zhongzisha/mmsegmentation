83dc2fe1129ed36c3141735b8d98787d0badf633
zhongzisha <zhongzisha@outlook.com>
2021-08-19 22:36:56 +0800

    add noaug

--- configs/_base_/datasets/gd_line512new_noaug.py
+++ configs/_base_/datasets/gd_line512new_noaug.py
@@ -1,71 +1,12 @@
-_base_ = [
-    # '../_base_/models/fcn_unet_s5-d16.py',
-    '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_40k.py'
-]
-
-# model settings
-norm_cfg = dict(type='SyncBN', requires_grad=True)
-model = dict(
-    type='EncoderDecoder',
-    pretrained=None,
-    backbone=dict(
-        type='UNet',
-        in_channels=3,
-        base_channels=64,
-        num_stages=5,
-        strides=(1, 1, 1, 1, 1),
-        enc_num_convs=(2, 2, 2, 2, 2),
-        dec_num_convs=(2, 2, 2, 2),
-        downsamples=(True, True, True, True),
-        enc_dilations=(1, 1, 1, 1, 1),
-        dec_dilations=(1, 1, 1, 1),
-        with_cp=False,
-        conv_cfg=None,
-        norm_cfg=norm_cfg,
-        act_cfg=dict(type='ReLU'),
-        upsample_cfg=dict(type='InterpConv'),
-        norm_eval=False),
-    decode_head=dict(
-        type='FCNHead',
-        in_channels=64,
-        in_index=4,
-        channels=64,
-        num_convs=1,
-        concat_input=False,
-        dropout_ratio=0.1,
-        num_classes=2,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=200.0)),
-    auxiliary_head=dict(
-        type='FCNHead',
-        in_channels=128,
-        in_index=3,
-        channels=64,
-        num_convs=1,
-        concat_input=False,
-        dropout_ratio=0.1,
-        num_classes=2,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
-    # model training and testing settings
-    train_cfg=dict(),
-    test_cfg=dict(mode='slide', crop_size=(256, 256), stride=(170, 170)))
-
-
 # dataset settings
 dataset_type = 'CustomDataset'
 data_root = 'data/gd_line512new'
 classes = ('bg', 'line')
 palette = [[0, 0, 0], [255, 255, 255]]
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+    mean=[0.0, 0.0, 0.0], std=[255.0, 255.0, 255.0], to_rgb=True)
 img_scale = (512, 512)
-crop_size = (256, 256)
+crop_size = (64, 64)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
@@ -95,8 +36,8 @@ test_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=2,
-    workers_per_gpu=2,
+    samples_per_gpu=4,
+    workers_per_gpu=4,
     train=dict(
         type='RepeatDataset',
         times=40000,
@@ -127,6 +68,3 @@ data = dict(
         ann_dir='val/annotations',
         split='val.txt',
         pipeline=test_pipeline))
-
-evaluation = dict(metric='mDice')
-optimizer = dict(lr=0.01)
--- configs/unet/unet_s5-d16_256x256_40k_line512new_lr0.005.py
+++ configs/unet/unet_s5-d16_256x256_40k_line512new_lr0.005.py
@@ -38,20 +38,7 @@ model = dict(
         norm_cfg=norm_cfg,
         align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=200.0)),
-    auxiliary_head=dict(
-        type='FCNHead',
-        in_channels=128,
-        in_index=3,
-        channels=64,
-        num_convs=1,
-        concat_input=False,
-        dropout_ratio=0.1,
-        num_classes=2,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
     # model training and testing settings
     train_cfg=dict(),
     test_cfg=dict(mode='slide', crop_size=(256, 256), stride=(170, 170)))
@@ -129,4 +116,4 @@ data = dict(
         pipeline=test_pipeline))
 
 evaluation = dict(metric='mDice')
-optimizer = dict(lr=0.01)
+optimizer = dict(lr=0.005)
