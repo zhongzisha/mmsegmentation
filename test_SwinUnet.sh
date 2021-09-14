90234dbe9f490e2e2369576c90307c387f30c79c
zhongzisha <zhongzisha@outlook.com>
2021-09-14 10:49:31 +0800

    add test_modified.py

---
+++ test_SwinUnet.sh
@@ -0,0 +1,200 @@
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_bs=24_lr=0.01_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnet_0.01.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_bs=24_lr=0.05_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnet_0.05.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_bs=24_lr=0.1_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnet_0.1.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_bs=24_lr=0.2_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnet_0.2.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_bs=24_lr=0.3_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnet_0.3.pkl
+
+#########################################################################################################
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V2_bs=24_lr=0.05_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV2_0.05.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V2_bs=24_lr=0.1_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV2_0.1.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V2_bs=24_lr=0.2_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV2_0.2.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V2_bs=24_lr=0.3_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV2_0.3.pkl
+
+
+#######################################################################################################
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V3_bs=24_lr=0.05_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV3_0.05.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V3_bs=24_lr=0.1_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV3_0.1.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V3_bs=24_lr=0.2_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV3_0.2.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V3_bs=24_lr=0.3_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV3_0.3.pkl
+
+############ 2 layers ###########################################################################################
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.05_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_0.05.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.1_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_0.1.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.2_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_0.2.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.3_tiny_224_4_/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_0.3.pkl
+
+############ 2 layers (END) ############################################################################################
+
+
+
+############ 3 layers ###################################
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.05_tiny_224_4_3layers/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_3layers_0.05.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.1_tiny_224_4_3layers/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_3layers_0.1.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.2_tiny_224_4_3layers/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_3layers_0.2.pkl
+
+CONFIG=unet_s5-d16_256x256_40k_line512new_lr0.01
+python test_with_debug.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+/media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+--work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+--eval mDice mIoU \
+--other_results_dir /media/ubuntu/Data/VesselSeg-Pytorch/gd/gd_lineseg_new_Swin_Unet_V4_bs=24_lr=0.3_tiny_224_4_3layers/predictions \
+--out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/result_SwinUnetV4_3layers_0.3.pkl
+
+############ 3 layers (END) ################################################################################
---
+++ test_Unet.sh
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+for ARCH in fcn_unet_s5-d16 deeplabv3_unet_s5-d16 pspnet_unet_s5-d16; do
+
+  for SIZE in 64x64 128x128 256x256; do
+
+    CONFIG=${ARCH}_${SIZE}_40k_gd_line512new_lr0.01
+    echo $CONFIG
+
+    CUDA_VISIBLE_DEVICES=1 python tools/test_modified.py \
+    /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/${CONFIG}.py \
+    /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/iter_40000.pth \
+    --work-dir /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/${CONFIG}/ \
+    --eval mDice mIoU \
+    --out /media/ubuntu/Temp/gd/mmsegmentation/work_dirs/unet_s5-d16_256x256_40k_line512new_lr0.01/result_${ARCH}_${SIZE}.pkl
+
+  done
+
+done
